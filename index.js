162ab0c28419ff486490269abb7d8f922c4950e0
Markmutuku <mark.mutuku@student.moringaschool.com>
2022-11-24 08:52:35 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(someValue){
   // Write your code here!
+if (someValue <= 400) {
+  return "This one is on me!";
+} else if (someValue >= 2000) {
+  return "I will gladly take your thirty bucks.";
+} else (someValue > 2500) ;{
+  return "No can do."
+}
+}
+  
+
+  
+function ternaryCheckCity(city){
+  // Write your code here!
+city === "NYC" ? "Ok, sounds good." : "No, go"
+
+
 }
 
-function ternaryCheckCity(){
+function switchOnCharmFromTip(tip){
+  switch (tip) {
+    case "generous":
+      console.log("Thank you so much.");
+      break;
+    case "not as generous":
+      console.log("Thank you.");
+      break;
+      case "if anything else":
+        console.log("Bye.");
+    default:
+      break;
+  }
   // Write your code here!
-}
 
-function switchOnCharmFromTip(){
-  // Write your code here!
 }
